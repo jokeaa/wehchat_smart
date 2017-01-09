6a66c25580cb7f938d800cb121e9cc3262ccf837
joke <k.joke.stone@gmail.com>
2017-01-09 17:22:21 +0800

    播放器只差进度条

--- pages/controlViva/controlViva.js
+++ pages/controlViva/controlViva.js
@@ -3,19 +3,52 @@
 Page({
  
   data:{
-    status:"play"
+    status:"play",
+    viva_now:"00:00",
+    viva_all:"00:00",
+    now_value:'0',
+    min_value:'0',
+    max_value:'100',
   },
-  audioPlay:function(e){
-      var thePage = this
-      wx.getBackgroundAudioPlayerState({
-          success: function(res){
-            console.log(res)
-            switch (res.status) {
-              case 2:
-                  wx.playBackgroundAudio({
+  seekViva:function(e){
+    this.setData({
+      status:"play"
+    })
+    var now_val = e.detail.value
+
+    wx.playBackgroundAudio({
+
                     dataUrl: 'http://qqma.tingge123.com:823/mp3/2015-11-09/1447026796.mp3',
                     success: function(res){
-                      console.log('success viva')
+                        wx.getBackgroundAudioPlayerState({
+                          success: function(res){
+                            var audio_res = res.duration
+                            switch (res.status){
+                              case 2:break;
+                              default:
+                              wx.seekBackgroundAudio({
+                                position: now_val/100*audio_res,
+                                success: function(res){
+                                  // success
+                                },
+                                fail: function() {
+                                  // fail
+                                },
+                                complete: function() {
+                                  // complete
+                                }
+                              })
+                            }
+                            
+                          },
+                          fail: function() {
+                            // fail
+                          },
+                          complete: function() {
+                            // complete
+                          }
+                        })
+                        
                     },
                     fail: function() {
                       // fail
@@ -24,18 +57,37 @@ Page({
                       // complete
                     }
                   })
-                  break;
+  },
+  location:function(e){
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function(res){
+        // console.log(res)
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  },
+  audioPlay:function(e){
+      var thePage = this
+      wx.getBackgroundAudioPlayerState({
+          success: function(res){
+            switch (res.status) {
               case 1:
                   wx.pauseBackgroundAudio()
                   thePage.setData({
                     status:"pause"
                   })
                   break;
-              case 0:
+              default:
                   wx.playBackgroundAudio({
                       dataUrl: 'http://qqma.tingge123.com:823/mp3/2015-11-09/1447026796.mp3',
                       success: function(res){
-                        console.log('success viva')
+                        
                       },
                       fail: function() {
                         // fail
